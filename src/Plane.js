a3c768fe77901a662cd1229fd94d01dc0c3497e3
Tagrand <thomas.a.grand@gmail.com>
2018-01-30 16:55:00 +0000

    try a new way of storing plane

--- src/Plane.js
+++ src/Plane.js
@@ -1,10 +1,13 @@
 function Plane() {
   this._Flying = true;
+
+  return {
+    land: function() { this._Flying = false; },
+    isFlying: function() { return this._Flying; }
+  }
 }
 
-Plane.prototype = {
-
-land: function() { this._Flying = false; },
-isFlying: function() { return this._Flying; }
-
-}
+// // Plane.prototype = {
+// //    land: function() { this._Flying = false; },
+//   isFlying: function() { return this._Flying; }
+// // }
